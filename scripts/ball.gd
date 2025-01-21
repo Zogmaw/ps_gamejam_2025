3093f73048b89bef524c1238df8bf68e0c7c12a5
ZRThornton <zach15682@gmail.com>
2025-01-20 21:53:47 -0500

    bit o work

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,16 +1,11 @@
 extends Node2D
 
-signal object_destroyed
+var isBall = true
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_area_2d_mouse_entered() -> void:
-	object_destroyed.emit()
-	queue_free()
-	
